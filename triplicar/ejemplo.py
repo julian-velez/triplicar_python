4a37761553531097a7ebf26dff378b7046f7fc03
julian-velez <60607869+julian-velez@users.noreply.github.com>
2025-06-25 09:28:06 -0500

    triplicar_python
    triplicar_python

---
+++ triplicar/ejemplo.py
@@ -0,0 +1,13 @@
+# Se crea una variable llamada 'curso' y se le asigna el texto "python"
+curso = "python"
+
+# Se crea otra variable llamada 'lenguaje'
+# A esta se le asigna el texto "python " (nota el espacio al final) repetido 2 veces.
+# Es decir, "python " * 2 → "python python "
+lenguaje  = "python "  * 2
+
+# Se imprime el contenido de la variable 'curso', que es simplemente "python"
+print(curso)
+
+# Se imprime el contenido de la variable 'lenguaje', que es "python python "
+print(lenguaje)
